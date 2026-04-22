5daaa8753a4726ca44ae1498e76b6f98595d2993
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-04-22 09:29:35 -0800

    Initial commit

---
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+public class AdventureTime
+{
+    public static void main()
+    {
+        //create user character
+
+
+       //start adventure
+
+
+
+        //present choices
+            //prompt user to make decision
+                //print results
+        //prompt next choice
+            //loop until end
+        //user info- user choice- display result- continue to next
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //end goal is a story completed
+    }
+}
